54d5b7b9a0df8084c5b8c40bbbf0a8dc1b350ed1
Aniket Bansal <96112371+Aniket021978@users.noreply.github.com>
2024-11-14 17:21:44 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,16 +1,15 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
-import login from '../images/login.png'
-import '../styles/hero.css'
-import '../styles/auth.css'
+import login from '../images/login.png';
+import '../styles/hero.css';
+import '../styles/auth.css';
 
 const Login = () => {
-
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     const location = useLocation();
 
     const validateEmail = (email) => {
@@ -20,81 +19,116 @@ const Login = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
         if (!email.trim()) {
             toast.error('Email is required');
-            return false;
+            return;
         }
+
         if (!validateEmail(email)) {
             toast.error('Invalid Email Format');
-            return false;
+            return;
         }
-        try {
-            const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/user/login`, {
-                email, password
-            });
-            if (res.data.success) {
-                localStorage.setItem('token',res.data.token);
-                localStorage.setItem('userId', res.data.user.id);
-                localStorage.setItem('username',res.data.user.name);
-                toast.success(res.data.message)
-                navigate(location.state || '/')
-                window.location.reload();
-            } else {
-                toast.error(res.data.message)
-            }
 
-        } catch (err) {
-            console.log(err)
+        if (!password.trim()) {
+            toast.error('Password is required');
+            return;
         }
-    }
+
+        try {
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/user/login`, { email, password });
+
+            if (res.data.success) {
+                localStorage.setItem('token', res.data.token);
+                localStorage.setItem('userId', res.data.user.id);
+                localStorage.setItem('username', res.data.user.name);
+                toast.success(res.data.message);
+                navigate(location.state || '/');
+                window.location.reload();
+            }
+        } catch (err) {
+            if (err.response) {
+                if (err.response.status === 404) {
+                    toast.error('User not registered');
+                } else if (err.response.status === 401) {
+                    toast.error('Incorrect password');
+                } else {
+                    toast.error(err.response.data.message || 'Login failed');
+                }
+            } else {
+                console.error('Error:', err);
+                toast.error('An error occurred. Please try again later.');
+            }
+        }
+    };
 
     useEffect(() => {
-        window.scrollTo(0, 0)
+        window.scrollTo(0, 0);
     }, []);
 
     return (
-        <>
-            <div className='marginStyle'>
-                <div class="container d-flex justify-content-center align-items-center">
-                    <div class="row border rounded-5 p-3 bg-white shadow box-area reverseCol">
-                        <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box">
-                            <div class="featured-image mb-3 animateImg">
-                                <img src={login} class="img-fluid" width={500} />
+        <div className='marginStyle'>
+            <div className="container d-flex justify-content-center align-items-center">
+                <div className="row border rounded-5 p-3 bg-white shadow box-area reverseCol">
+                    <div className="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box">
+                        <div className="featured-image mb-3 animateImg">
+                            <img src={login} className="img-fluid" width={500} alt="Login" />
+                        </div>
+                    </div>
+                    <form className="col-md-6 right-box" onSubmit={handleSubmit}>
+                        <div className="row align-items-center">
+                            <div className="header-text mb-4">
+                                <h2>Welcome</h2>
+                                <p>We are happy to have you back!</p>
+                            </div>
+                            <div className="input-group d-flex align-items-center mb-3">
+                                <div className="form-outline flex-fill mb-0">
+                                    <input
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        required
+                                        type="email"
+                                        placeholder="Your email ID"
+                                        className="form-control"
+                                    />
+                                </div>
+                            </div>
+                            <div className="input-group d-flex flex-row align-items-center mb-3">
+                                <div className="form-outline flex-fill mb-0">
+                                    <input
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                        type="password"
+                                        placeholder="Your password"
+                                        className="form-control"
+                                    />
+                                </div>
+                            </div>
+                            <div className="d-flex flex-row align-items-center mt-4">
+                                <div className="form-outline flex-fill mb-0">
+                                    <button
+                                        className="btn btn-lg text-white"
+                                        type="submit"
+                                        style={{ backgroundColor: 'blueviolet', width: '100%' }}
+                                    >
+                                        Login
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="d-flex flex-row align-items-center my-3">
+                                <div className="form-outline flex-fill mb-0">
+                                    <Link to='/register' className="btn btn-outline-dark btn-lg btn-block" style={{ width: '100%' }}>
+                                        Register
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                        <form class="col-md-6 right-box">
-                            <div class="row align-items-center">
-                                <div class="header-text mb-4">
-                                    <h2>Welcome</h2>
-                                    <p>We are happy to have you back !</p>
-                                </div>
-                                <div class="input-group d-flex  align-items-center mb-3">
-                                    <div class="form-outline flex-fill mb-0">
-                                        <input value={email} onChange={(e) => setEmail(e.target.value)} required type="email" placeholder='Your email ID' class="form-control" />
-                                    </div>
-                                </div>
-                                <div class="input-group d-flex flex-row align-items-center mb-3">
-                                    <div class="form-outline flex-fill mb-0">
-                                        <input value={password} onChange={(e) => setPassword(e.target.value)} required type="password" placeholder='Your password' class="form-control" />
-                                    </div>
-                                </div>
-                                <div class="d-flex flex-row align-items-center mt-4 ">
-                                    <div class="form-outline flex-fill mb-0">
-                                        <button class="btn btn-lg  text-white" type="button" onClick={handleSubmit} style={{ backgroundColor: 'blueviolet', width: '100%' }} >Login</button>
-                                    </div>
-                                </div>
-                                <div class="d-flex flex-row align-items-center my-3 ">
-                                    <div class="form-outline flex-fill mb-0 " >
-                                        <Link to='/register' class="btn btn-outline-dark btn-lg btn-block" style={{ width: '100%' }} type="button">Register</Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div >
-            </div >
-        </>
-    )
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
