be052c1079d6b29e59c3ce172e8cb880572bfa38
Aniket Bansal <96112371+Aniket021978@users.noreply.github.com>
2024-11-14 15:07:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,11 +18,7 @@ if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir);
 }
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/uploads', express.static(uploadDir));
 
